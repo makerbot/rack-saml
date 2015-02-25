aa54043c3df59bf7e08cf41e50acdcbd4f9e511f
Zachary Elliott <zach@nyu.edu>
2015-02-24 23:55:15 -0500

    Masking saml_settings config variable
    This is being masked to work around the use of 'global' state in the
    adapter.

--- lib/rack/saml/misc/onelogin_setting.rb
+++ lib/rack/saml/misc/onelogin_setting.rb
@@ -3,10 +3,10 @@ module Rack
     module OneloginSetting
       require 'ruby-saml'
 
-      def saml_settings
+      def saml_settings(config=@config)
         settings = OneLogin::RubySaml::Settings.new
-        settings.assertion_consumer_service_url = @config['assertion_consumer_service_uri']
-        settings.issuer = @config['saml_sp']
+        settings.assertion_consumer_service_url = config['assertion_consumer_service_uri']
+        settings.issuer = config['saml_sp']
         settings.idp_sso_target_url = @metadata['saml2_http_redirect']
         settings.idp_cert = @metadata['certificate']
         settings.name_identifier_format = "urn:oasis:names:tc:SAML:2.0:nameid-format:transient"
