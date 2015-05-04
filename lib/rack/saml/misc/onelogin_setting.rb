4445cf65094d28b181d70243351001a19ccf83a4
Zachary Elliott <zach@nyu.edu>
2015-05-04 17:45:57 -0400

    Adding configuration for signed assertions

--- lib/rack/saml/misc/onelogin_setting.rb
+++ lib/rack/saml/misc/onelogin_setting.rb
@@ -11,6 +11,20 @@ module Rack
         settings.idp_cert = @metadata['certificate']
         settings.name_identifier_format = "urn:oasis:names:tc:SAML:2.0:nameid-format:transient"
         #settings.authn_context = "urn:oasis:names:tc:SAML:2.0:ac:classes:PasswordProtectedTransport"
+
+        settings.certificate = ::File.read(@config['sp_cert'])
+        settings.private_key = ::File.read(@config['sp_key'])
+
+        settings.security[:authn_requests_signed] = true
+        settings.security[:logout_requests_signed] = true
+        settings.security[:logout_responses_signed] = true
+        settings.security[:metadata_signed] = true
+
+        settings.security[:digest_method] = XMLSecurity::Document::SHA1
+        settings.security[:signature_method] = XMLSecurity::Document::RSA_SHA1
+
+        settings.security[:embed_sign] = true
+
         settings
       end
     end
